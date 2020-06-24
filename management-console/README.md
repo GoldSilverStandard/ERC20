331f9db811e1a378f7af35daa5256bcc5215f01c
Justin Taylor <s.justin.taylor@gmail.com>
2020-06-24 14:41:56 +1000

    send transaction works

--- management-console/README.md
+++ management-console/README.md
@@ -7,8 +7,38 @@
 - `transfer`
 - `transferFrom`
 - `approve`
+- `totalSupply`
 - `sendTransaction`
+
+## Installation
+
+Install the token cli
+
+```bash
+git clone https://github.com/GoldSilverStandard/ERC20.git
+cd ERC20/management-console
+yarn install
+cp example.env .env
+```
+
+You will want to make sure that the values in the newly created `.env` file are correct.
+An example `.env` file is provided below. `PROVIDER` should be a secure websockets provider endpoint. `SENDER_ADDRESS` is the address you will be sending the transaction from. This is used for nonce management.
+
+```
+PROVIDER="wss://mainnet.infura.io/ws/v3/YOUR-PROJECT-ID"
+SENDER_ADDRESS="0x8325aBBB91775651CE44C87AcC80c47778c91814"
+```
 
 ## Usage
 
-`token-cli mint`
+Ensure you are in the `ERC20/management-console` directory.
+
+Use like any other console command. For example to create a mint transaction: `./bin/token-cli mint`. This will ask several questions, and generate the transaction.
+You can then use the output with a tool like [EthOffline](https://ethjs.github.io/offline/) to create a signed transaction. The `sendTransaction` command will send a signed transaction to the network.
+
+## Test data, remove
+
+0x896756178b309e42ea606d992AbD5d225755bB46
+
+signed ropsten tx:
+0xf8ab818885012a05f2008307a1209484cbc5ee75a62afb41bce21680151ae2ec68eff380b844095ea7b3000000000000000000000000c5eb6a07018d267111050c41f3862b2bccbdc86f00000000000000000000000000000000000000000000000000000000000000051ba0a201f0d96f0391dbdb2934410eec0cf322a9503e1086c837fd45747e57e29be1a041a50faa46e7e90d55f723aa87c3f8008dedc39fd2aebb292ac19101076412ea
--- management-console/index.js
+++ management-console/index.js
@@ -1,9 +1,11 @@
-require("dotenv").config()
+require("dotenv").config();
 const minimist = require("minimist");
 const inquirer = require("./prompts");
 const abi = require("./abi");
-const Web3 = require('web3');
-const web3 = new Web3(process.env.PROVIDER)
+const Web3 = require("web3");
+const web3 = new Web3(process.env.PROVIDER);
+const tokenContract = new web3.eth.Contract(abi);
+const BigNumber = require("bignumber.js");
 
 module.exports = async () => {
   const args = minimist(process.argv.slice(2));
@@ -20,32 +22,116 @@ module.exports = async () => {
     switch (cmd) {
       case "mint": {
         const mintOptions = await inquirer.askMintInputs();
-        // Get nonce
-            const nonce = await 
+        // Create unsigned transaction abi
+        const txData = tokenContract.methods
+          .mint(
+            mintOptions.address,
+            web3.utils.utf8ToHex(mintOptions.location),
+            web3.utils.utf8ToHex(mintOptions.serial),
+            BigNumber(mintOptions.value).times(10000).toString()
+          )
+          .encodeABI();
+        console.log(
+          "Parameters: \n",
+          mintOptions,
+          "\n\nThe mint transaction prepared for signing: \n\n",
+          await generateTx(txData)
+        );
         break;
       }
       case "burn": {
         const burnOptions = await inquirer.askBurnInputs();
-        console.log(burnOptions);
+        const txData = tokenContract.methods
+          .burn(
+            web3.utils.utf8ToHex(burnOptions.location),
+            web3.utils.utf8ToHex(burnOptions.serial)
+          )
+          .encodeABI();
+        console.log(
+          "Parameters: \n",
+          burnOptions,
+          "\n\nThe burn transaction prepared for signing: \n\n",
+          await generateTx(txData)
+        );
         break;
       }
       case "transfer": {
         const transferOptions = await inquirer.askTransferInputs();
-        console.log(transferOptions);
+        const txData = tokenContract.methods
+          .transfer(transferOptions.to, transferOptions.value)
+          .encodeABI();
+        console.log(
+          "Parameters: \n",
+          transferOptions,
+          "\n\nThe transfer transaction prepared for signing: \n\n",
+          await generateTx(txData)
+        );
         break;
       }
       case "transferFrom": {
         const transferFromOptions = await inquirer.askTransferFromInputs();
-        console.log(transferFromOptions);
+        const txData = tokenContract.methods
+          .transferFrom(
+            transferFromOptions.addressFrom,
+            transferFromOptions.addressTo,
+            transferFromOptions.value
+          )
+          .encodeABI();
+        console.log(
+          "Parameters: \n",
+          transferFromOptions,
+          "\n\nThe transfer from transaction prepared for signing: \n\n",
+          await generateTx(txData)
+        );
         break;
       }
       case "approve": {
         const approveOptions = await inquirer.askApproveInputs();
-        console.log(approveOptions);
+        const txData = tokenContract.methods
+          .approve(approveOptions.spender, approveOptions.value)
+          .encodeABI();
+        console.log(
+          "Parameters: \n",
+          approveOptions,
+          "\n\nThe approve spending transaction prepared for signing: \n\n",
+          await generateTx(txData)
+        );
+        break;
+      }
+      case "sendTransaction": {
+        const tx = await inquirer.askSendTransactionInputs();
+        console.log("Sending raw transaction....");
+        await web3.eth
+          .sendSignedTransaction(tx.transaction)
+          .on("receipt", (receipt) => {
+            console.log("Transaction receipt received: \n", receipt);
+          });
+        break;
+      }
+      case "totalSupply": {
+        const totalSupply = await tokenContract.methods.totalSupply().call();
+        console.log(
+          "Total token supply is:",
+          BigNumber(totalSupply).div(10000).toString()
+        );
+        break;
+      }
+      default: {
+        console.log("Unknown command, exiting");
         break;
       }
     }
   } catch (e) {
     console.log(e);
+    process.exit();
   }
+  process.exit();
 };
+
+async function generateTx(data, nonce) {
+  return {
+    nonce: await web3.eth.getTransactionCount(process.env.SENDER_ADDRESS),
+    value: "0",
+    data,
+  };
+}
